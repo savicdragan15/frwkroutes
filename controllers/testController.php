74894d97832a93c92920c932647c42f5e0c55509
savicdragan15 <savicdragan2707@gmail.com>
2016-09-26 19:23:41 +0200

    - add Blade template engine

--- controllers/testController.php
+++ controllers/testController.php
@@ -1,4 +1,5 @@
 <?php
+use duncan3dc\Laravel\BladeInstance;
 class testController extends baseController
 {
     public function __construct()
@@ -13,6 +14,18 @@ class testController extends baseController
         );
         
         var_dump($this->_products->like("*",$condition));
+    }
+    
+    public function test(){
+        
+        $views ="C:/wamp/www/all_shine_out/views";
+        $cache = __DIR__ . '/cache';
+        
+        $blade = new BladeInstance($views, $cache);
+
+        echo $blade->render("test",['name' => 'Dragan']);
+        
+     
     }
 }
 
